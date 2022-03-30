611bfae726af737e68d459ff1261e282ccb57db7
ManjuBanu <manjubanu0823@gmail.com>
2022-03-31 00:26:46 +0530

    Updates

---
+++ precache-manifest.11025191b136d1d3ecfcdf88a687e1ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8fc84ddc2b5de5a5209ab14e2fe7e94",
+    "url": "/ReactCard/index.html"
+  },
+  {
+    "revision": "855cd1eedd8cb137888b",
+    "url": "/ReactCard/static/css/2.d694ff75.chunk.css"
+  },
+  {
+    "revision": "26c33b1e8507ee643d5a",
+    "url": "/ReactCard/static/css/main.ec4c4fe5.chunk.css"
+  },
+  {
+    "revision": "855cd1eedd8cb137888b",
+    "url": "/ReactCard/static/js/2.6eb2eb18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactCard/static/js/2.6eb2eb18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c33b1e8507ee643d5a",
+    "url": "/ReactCard/static/js/main.248771e9.chunk.js"
+  },
+  {
+    "revision": "185eafb079f55a34adc7",
+    "url": "/ReactCard/static/js/runtime-main.be7ae844.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactCard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "515d84cd191ce53fe25eeb45ca619370",
+    "url": "/ReactCard/static/media/manju.515d84cd.jpg"
+  }
+]);
